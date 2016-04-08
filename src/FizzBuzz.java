03d939003cb13f7ca7386dbfd11a7ef0b5320422
Kerry Buckley <kerryjbuckley@gmail.com>
2016-04-08 14:43:21 +0100

    Initial commit (basic FizzBuzz class)

---
+++ src/FizzBuzz.java
@@ -0,0 +1,13 @@
+public class FizzBuzz {
+    public String say(final int number) {
+        if (number % 15 == 0 ) {
+            return "fizzbuzz";
+        } else if (number % 3 == 0) {
+            return "fizz";
+        } else if (number % 5 == 0) {
+            return "buzz";
+        } else {
+            return Integer.toString(number);
+        }
+    }
+}
