00f0fb6c3647f2174339dcebb3b8a5e86cdb7bc7
Kerry Buckley <kerryjbuckley@gmail.com>
2016-04-08 15:56:10 +0100

    Print out a few more test cases

--- test/Main.java
+++ test/Main.java
@@ -2,5 +2,13 @@ public class Main {
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
         System.out.println(fizzBuzz.say(1));
+        System.out.println(fizzBuzz.say(2));
+        System.out.println(fizzBuzz.say(3));
+        System.out.println(fizzBuzz.say(4));
+        System.out.println(fizzBuzz.say(5));
+        System.out.println(fizzBuzz.say(6));
+        System.out.println(fizzBuzz.say(7));
+        System.out.println(fizzBuzz.say(10));
+        System.out.println(fizzBuzz.say(15));
     }
 }
