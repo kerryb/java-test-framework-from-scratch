013daa583a1994a05c73b107fb24a655b570d89d
Kerry Buckley <kerryjbuckley@gmail.com>
2016-04-08 16:36:05 +0100

    Raise exception on failure

--- test/Main.java
+++ test/Main.java
@@ -12,34 +12,32 @@ public class Main {
     }
 
     private static void converts_3_to_fizz() {
-        testValue(fizzBuzz.say(3), "fizz");
+        assertEqual(fizzBuzz.say(3), "fizz");
     }
 
     private static void converts_5_to_buzz() {
-        testValue(fizzBuzz.say(5), "buzz");
+        assertEqual(fizzBuzz.say(5), "buzz");
     }
 
     private static void converts_multiples_of_three_to_fizz() {
-        testValue(fizzBuzz.say(6), "fizz");
+        assertEqual(fizzBuzz.say(6), "fizz");
     }
 
     private static void converts_multiples_of_five_to_buzz() {
-        testValue(fizzBuzz.say(10), "buzz");
+        assertEqual(fizzBuzz.say(10), "buzz");
     }
 
     private static void converts_multiples_of_three_and_five_to_fizzbuzz() {
-        testValue(fizzBuzz.say(15), "fizzbuzz");
+        assertEqual(fizzBuzz.say(15), "fizzbuzz");
     }
 
     private static void leaves_other_numbers_alone() {
-        testValue(fizzBuzz.say(1), "1");
+        assertEqual(fizzBuzz.say(1), "1");
     }
 
-    private static void testValue(String value, String expected) {
-        if (value.equals(expected)) {
-            System.out.println("OK");
-        } else {
-            System.out.println("FAIL: Expected '" + expected + "'; got '" + value + "'.");
+    private static void assertEqual(String value, String expected) {
+        if (!value.equals(expected)) {
+            throw new RuntimeException("Expected '" + expected + "'; got '" + value + "'.");
         }
     }
 }
