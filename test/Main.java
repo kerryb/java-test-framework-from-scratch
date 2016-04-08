dc90839058232cacda7e1c81046385962402ddd9
Kerry Buckley <kerryjbuckley@gmail.com>
2016-04-08 16:33:03 +0100

    Extract tests to helpfully-named methods

--- test/Main.java
+++ test/Main.java
@@ -1,15 +1,38 @@
 public class Main {
+    private static FizzBuzz fizzBuzz;
+
     public static void main(String[] args) {
-        FizzBuzz fizzBuzz = new FizzBuzz();
-        testValue(fizzBuzz.say(1), "1");
-        testValue(fizzBuzz.say(2), "2");
+        fizzBuzz = new FizzBuzz();
+        converts_3_to_fizz();
+        converts_5_to_buzz();
+        converts_multiples_of_three_to_fizz();
+        converts_multiples_of_five_to_buzz();
+        converts_multiples_of_three_and_five_to_fizzbuzz();
+        leaves_other_numbers_alone();
+    }
+
+    private static void converts_3_to_fizz() {
         testValue(fizzBuzz.say(3), "fizz");
-        testValue(fizzBuzz.say(4), "4");
+    }
+
+    private static void converts_5_to_buzz() {
         testValue(fizzBuzz.say(5), "buzz");
+    }
+
+    private static void converts_multiples_of_three_to_fizz() {
         testValue(fizzBuzz.say(6), "fizz");
-        testValue(fizzBuzz.say(7), "7");
+    }
+
+    private static void converts_multiples_of_five_to_buzz() {
         testValue(fizzBuzz.say(10), "buzz");
+    }
+
+    private static void converts_multiples_of_three_and_five_to_fizzbuzz() {
         testValue(fizzBuzz.say(15), "fizzbuzz");
+    }
+
+    private static void leaves_other_numbers_alone() {
+        testValue(fizzBuzz.say(1), "1");
     }
 
     private static void testValue(String value, String expected) {
