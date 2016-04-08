35cd30fbdcf8ced02927417d65245a13bb1f93a4
Kerry Buckley <kerryjbuckley@gmail.com>
2016-04-08 16:05:03 +0100

    Print out expected as well as actual result

--- test/Main.java
+++ test/Main.java
@@ -1,18 +1,18 @@
 public class Main {
     public static void main(String[] args) {
         FizzBuzz fizzBuzz = new FizzBuzz();
-        testValue(fizzBuzz.say(1));
-        testValue(fizzBuzz.say(2));
-        testValue(fizzBuzz.say(3));
-        testValue(fizzBuzz.say(4));
-        testValue(fizzBuzz.say(5));
-        testValue(fizzBuzz.say(6));
-        testValue(fizzBuzz.say(7));
-        testValue(fizzBuzz.say(10));
-        testValue(fizzBuzz.say(15));
+        testValue(fizzBuzz.say(1), "1");
+        testValue(fizzBuzz.say(2), "2");
+        testValue(fizzBuzz.say(3), "fizz");
+        testValue(fizzBuzz.say(4), "4");
+        testValue(fizzBuzz.say(5), "buzz");
+        testValue(fizzBuzz.say(6), "fizz");
+        testValue(fizzBuzz.say(7), "7");
+        testValue(fizzBuzz.say(10), "buzz");
+        testValue(fizzBuzz.say(15), "fizzbuzz");
     }
 
-    private static void testValue(String value) {
-        System.out.println(value);
+    private static void testValue(String value, String expected) {
+        System.out.println("Expected '" + value + "'; got '" + value + "'.");
     }
 }
