c6f6fb11061b48b3a70df99857648a5d4f841a09
Kerry Buckley <kerryjbuckley@gmail.com>
2016-04-19 20:43:22 +0530

    Remove noise

--- test/TestSet.java
+++ test/TestSet.java
@@ -8,36 +8,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class TestSet {
-    protected void setup() {}
-
-    public void run() throws Throwable {
-        Method[] methods = getClass().getDeclaredMethods();
-        int tests = 0;
-        List<TestFailure> failures = new ArrayList<>();
-        for (Method method: methods) {
-            if (method.isAnnotationPresent(Test.class)) {
-                tests++;
-                try {
-                    setup();
-                    method.invoke(this);
-                } catch (IllegalAccessException e) {
-                    e.printStackTrace();
-                } catch (InvocationTargetException e) {
-                    failures.add(new TestFailure(method.getName(), e.getCause().getMessage()));
-                }
-            }
-        }
-        System.out.println(pluralise(tests, "test") + "; " + pluralise(failures.size(), "failure") + ".");
-        if (!failures.isEmpty()) {
-            for (TestFailure failure: failures) {
-                failure.report();
-            }
-            System.exit(1);
-        }
-    }
-
     @Retention(RetentionPolicy.RUNTIME)
-    @Target(ElementType.METHOD)
     protected @interface Test {}
 
     protected class TestFailure {
@@ -55,13 +26,35 @@ public class TestSet {
         }
     }
 
-    protected void assertEqual(final String value, final String expected) {
-        if (!value.equals(expected)) {
-            throw new RuntimeException("Expected '" + expected + "'; got '" + value + "'.");
+    public void run() throws Throwable {
+        Method[] methods = getClass().getDeclaredMethods();
+        int tests = 0;
+        List<TestFailure> failures = new ArrayList<>();
+        for (Method method: methods) {
+            if (method.isAnnotationPresent(Test.class)) {
+                tests++;
+                try {
+                    setup();
+                    method.invoke(this);
+                } catch (InvocationTargetException e) {
+                    failures.add(new TestFailure(method.getName(), e.getCause().getMessage()));
+                }
+            }
+        }
+        System.out.println("Tests run: " + tests + "; tests failed: " + failures.size());
+        if (!failures.isEmpty()) {
+            for (TestFailure failure: failures) {
+                failure.report();
+            }
+            System.exit(1);
         }
     }
 
-    protected String pluralise(final int number, final String label) {
-        return number + " " + label + (number == 1 ? "" : "s");
+    protected void setup() {}
+
+    protected void assertEqual(final Object value, final Object expected) {
+        if (!value.equals(expected)) {
+            throw new RuntimeException("Expected '" + expected + "'; got '" + value + "'.");
+        }
     }
 }
